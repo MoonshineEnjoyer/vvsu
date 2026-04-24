37c87d7518c3970cd77cc8eb9ec99fbe6f7b0a41
MoonshineEnjoyer <bobovich20@bk.ru>
2026-04-25 09:14:25 +1000

    Update expr_tree.py

--- lab - mathlog/expr_tree.py
+++ lab - mathlog/expr_tree.py
@@ -1,431 +1,198 @@
-from enum import Enum, IntEnum
-
-class ExprTreeType(Enum):
-    VAR = 0
-    CONST = 1
-    NEG = 2
-    BIN = 3
-
-
-class ExprTree():
-    def get_type(self):
-        raise
-
-    def print(self, f):
-        pass
-
-    def copy_tree(self):
-        pass
-
-
-class VarExpr(ExprTree):
-    def __init__(self, var_name):
-        self.var_name = var_name
-
-    def get_type(self):
-        return ExprTreeType.VAR
-
-    def print(self, f):
-        f.write(self.var_name)
-
-    def copy_tree(self):
-        return VarExpr(self.var_name)
-
-
-class ConstExpr(ExprTree):
-    def __init__(self, const_value):
-        self.const_value = const_value
-
-    def get_type(self):
-        return ExprTreeType.CONST
-
-    def print(self, f):
-        f.write(int(self.const_value))
-
-    def copy_tree(self):
-        return ConstExpr(self.const_value)
-
-
-class NegExprTree(ExprTree):
-    def __init__(self, arg):
-        self.arg = arg
-
-    def get_type(self):
-        return ExprTreeType.NEG
-
-    def print(self, f):
-        f.write("~")
-        add_brackets = False
-        if self.arg.get_type() != ExprTreeType.VAR and self.arg.get_type() != ExprTreeType.CONST and self.arg.get_type() != ExprTreeType.NEG:
-            add_brackets = True
-        if add_brackets:
-            f.write("(")
-        self.arg.print(f)
-        if add_brackets:
-            f.write(")")
-
-    def copy_tree(self):
-        return NegExprTree(self.arg.copy_tree())
-
-
-class BinaryExprTree(ExprTree):
-    def __init__(self, op, arg1, arg2):
-        self.op = op
-        self.arg1 = arg1
-        self.arg2 = arg2
-
-    def get_type(self):
-        return ExprTreeType.BIN
-
-    def print(self, f):
-        add_brackets = False
-        # если приоритет выражения выше, чем приоритет подвыражения
-        if (self.arg1.get_type() == ExprTreeType.BIN) and (self.arg1.op > self.op or (self.arg1.op == BinaryOperation.IMPL and self.op == BinaryOperation.IMPL)):
-            add_brackets = True
-        if add_brackets:
-            f.write("(")
-        self.arg1.print(f)
-        if add_brackets:
-            f.write(")")
-        f.write(" ")
-        if self.op == BinaryOperation.CONJ:
-            f.write("&")
-        elif self.op == BinaryOperation.DISJ:
-            f.write("|")
-        elif self.op == BinaryOperation.IMPL:
-            f.write("->")
-        elif self.op == BinaryOperation.EQUIV:
-            f.write("<->")
-        f.write(" ")
-        add_brackets = False
-        if (self.arg2.get_type() == ExprTreeType.BIN) and (self.arg2.op > self.op or (self.arg2.op == BinaryOperation.IMPL and self.op == BinaryOperation.IMPL)):
-            add_brackets = True
-        if add_brackets:
-            f.write("(")
-        self.arg2.print(f)
-        if add_brackets:
-            f.write(")")
-
-    def copy_tree(self):
-        return BinaryExprTree(self.op, self.arg1.copy_tree(), self.arg2.copy_tree())
-
-
-class TokenType(Enum):
-    EMPTY = 0
-    VAR = 1
-    CONST = 2
-    NEG = 3
-    CONJ = 4
-    DISJ = 5
-    IMPL = 6
-    EQUIV = 7
-    OPEN_BR = 8
-    CLOSE_BR = 9
-
-
-class Token():
-    def token_type(self):
-        raise
-
-
-class EmptyToken(Token):
-    def token_type(self):
-        return TokenType.EMPTY
-
-
-class VarToken(Token):
-    def token_type(self):
-        return TokenType.VAR
-
-    def __init__(self, s, index):
-        self.s = s
-        self.index = index
-
-
-class ConstToken(Token):
-    def token_type(self):
-        return TokenType.CONST
-
-    def __init__(self, val, index):
-        self.val = val
-        self.index = index
-
-
-class UnaryOperationToken(Token):
-    def token_type(self):
-        return TokenType.NEG
-
-    def __init__(self, index):
-        self.index = index
-
-
-class BinaryOperation(IntEnum):
-    CONJ = 1
-    DISJ = 2
-    IMPL = 3
-    EQUIV = 4
-
-
-class BinaryOperationToken(Token):
-    def token_type(self):
-        return {
-            BinaryOperation.CONJ: TokenType.CONJ,
-            BinaryOperation.DISJ: TokenType.DISJ,
-            BinaryOperation.IMPL: TokenType.IMPL,
-            BinaryOperation.EQUIV: TokenType.EQUIV
-        }[self.op]
-
-    def __init__(self, op, index):
-        self.op = op
-        self.index = index
-
-
-class OpenBracketToken(Token):
-    def token_type(self):
-        return TokenType.OPEN_BR
-
-    def __init__(self, index):
-        self.index = index
-
-
-class CloseBracketToken(Token):
-    def token_type(self):
-        return TokenType.CLOSE_BR
-
-    def __init__(self, index):
-        self.index = index
-
-
-class State(Enum):
-    START = 0
-    LETTER = 1
-    DIGIT = 2
-    TILDE = 3
-    DISJ = 4
-    CONJ = 5
-    DASH = 6
-    LESS = 7
-    LESS_DASH = 8
-    OPEN_BR = 9
-    CLOSE_BR = 10
-    DASH_GREATER = 11
-    LESS_DASH_GREATER = 12
-
-
-class ELexerError(Exception):
-    def __init__(self, cur, msg):
-        self.cur = cur
-        self.msg = msg
-
-    def message(self):
-        return self.msg + " at position " + str(self.cur + 1)
-
-class EParserError(ELexerError):
-    pass
-
-class Lexer():
-    def __init__(self, s):
-        self.s = s
-        self.cur = 0
-        self.next_token()
-
-    def next_token(self):
-        state = State.START
-        letter = None
-        digit = None
-        index = None  # индекс начала лексемы
-        while True:
-            if state == State.START:
-                if self.cur == len(self.s):
-                    self.token = EmptyToken()
-                    return
-                else:
-                    ch = self.s[self.cur]
-                    if ch.isalpha():  # если это буква
-                        letter = ch
-                        index = self.cur
-                        state = State.LETTER
-                    elif ch == '0' or ch == '1':
-                        digit = ch
-                        index = self.cur
-                        state = State.LETTER
-                    elif ch == '~':
-                        index = self.cur
-                        state = State.TILDE
-                    elif ch == '+' or ch == '|':
-                        index = self.cur
-                        state = State.DISJ
-                    elif ch == '&' or ch == '*':
-                        index = self.cur
-                        state = State.CONJ
-                    elif ch == '=' or ch == '-':
-                        index = self.cur
-                        state = State.DASH
-                    elif ch == '<':
-                        index = self.cur
-                        state = State.LESS
-                    elif ch == '(':
-                        index = self.cur
-                        state = State.OPEN_BR
-                    elif ch == ')':
-                        index = self.cur
-                        state = State.CLOSE_BR
-                    elif ch == ' ':
-                        # state = State.START
-                        pass
-                    else:
-                        raise ELexerError(self.cur, "Unexpected symbol")
-            elif state == State.LETTER:
-                if self.cur == len(self.s) or not self.s[self.cur].isalpha() and not self.s[self.cur].isdigit():
-                    self.token = VarToken(letter, index)
-                    return
-                elif self.s[self.cur].isalpha():
-                    raise ELexerError(self.cur, "Extra letter")
-                elif self.s[self.cur].isdigit():
-                    raise ELexerError(self.cur, "Unexpected digit")
-            elif state == State.DIGIT:
-                if self.cur == len(self.s) or not self.s[self.cur].isalpha() and not self.s[self.cur].isdigit():
-                    self.token = ConstToken(digit == "1", index)
-                    return
-            elif state == State.TILDE:
-                self.token = UnaryOperationToken(index)
-                return
-            elif state == State.DISJ:
-                self.token = BinaryOperationToken(BinaryOperation.DISJ, index)
-                return
-            elif state == State.CONJ:
-                self.token = BinaryOperationToken(BinaryOperation.CONJ, index)
-                return
-            elif state == State.DASH:
-                if self.cur == len(self.s):
-                    raise ELexerError(self.cur, "Unexpected end of line")
-                elif self.s[self.cur] == '>':
-                    state = State.DASH_GREATER
-                else:
-                    raise ELexerError(self.cur, "Unexpected symbol")
-            elif state == State.DASH_GREATER:
-                self.token = BinaryOperationToken(BinaryOperation.IMPL, index)
-                return
-            elif state == State.LESS:
-                if self.cur == len(self.s):
-                    raise ELexerError(self.cur, "Unexpected end of line")
-                elif self.s[self.cur] == '-' or self.s[self.cur] == '=':
-                    state = State.LESS_DASH
-                else:
-                    raise ELexerError(self.cur, "Unexpected symbol")
-            elif state == State.LESS_DASH:
-                if self.cur == len(self.s):
-                    raise ELexerError(self.cur, "Unexpected end of line")
-                elif self.s[self.cur] == '>':
-                    state = State.LESS_DASH_GREATER
-                else:
-                    raise ELexerError(self.cur, "Unexpected symbol")
-            elif state == State.LESS_DASH_GREATER:
-                self.token = BinaryOperationToken(BinaryOperation.EQUIV, index)
-                return
-            elif state == State.OPEN_BR:
-                self.token = OpenBracketToken(index)
-                return
-            elif state == State.CLOSE_BR:
-                self.token = CloseBracketToken(index)
-                return
-
-            self.cur = self.cur + 1
-
-    def get_token(self):
-        return self.token
-
-    def get_cur(self):
-        return self.cur
-
-
-class Parser():
-    def __init__(self, s):
-        self.s = s
-        self.lexer = Lexer(s)
-
-    def parse(self):
-        # token = self.lexer.get_token()
-        # print(token.token_type())
-        # while token.token_type() != TokenType.EMPTY:
-        #     self.lexer.next_token()
-        #     token = self.lexer.get_token()
-        #     print(token.token_type())
-        et = self.parse_equiv()
-        if self.lexer.get_token().token_type() != TokenType.EMPTY:
-            raise EParserError(self.lexer.get_cur(), "End of expression expected")
-        return et
-
-    def parse_equiv(self):
-        arg1 = self.parse_impl()
-        while self.lexer.get_token().token_type() == TokenType.EQUIV:
-            self.lexer.next_token()
-            arg2 = self.parse_impl()
-            et = BinaryExprTree(BinaryOperation.EQUIV, arg1, arg2)
-            arg1 = None
-            arg1 = et
-        return arg1
-
-    def parse_impl(self):
-        arg1 = self.parse_disj()
-        token_type = self.lexer.get_token().token_type()
-        if token_type == TokenType.IMPL:
-            self.lexer.next_token()
-            arg2 = self.parse_impl()
-            et = BinaryExprTree(BinaryOperation.IMPL, arg1, arg2)
-            # импликация право-ассоциативна: A -> B -> C = A -> (B -> C)
-            return et
+import time
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None  # Для Queue (двусвязный список для O(1) с обоих концов)
+
+class Stack:
+    """Стек на односвязном списке."""
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Pop from empty stack")
+        value = self.head.value
+        self.head = self.head.next
+        self.size -= 1
+        return value
+
+    def peek(self):
+        return self.head.value if self.head else None
+
+    def is_empty(self):
+        return self.head is None
+
+class Queue:
+    """Очередь на двусвязном списке для обеспечения O(1) операций."""
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.head = self.tail = new_node
         else:
-            return arg1
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
 
-    def parse_disj(self):
-        arg1 = self.parse_conj()
-        while self.lexer.get_token().token_type() == TokenType.DISJ:
-            self.lexer.next_token()
-            arg2 = self.parse_conj()
-            et = BinaryExprTree(BinaryOperation.DISJ, arg1, arg2)
-            arg1 = None
-            arg1 = et
-        return arg1
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Dequeue from empty queue")
+        value = self.head.value
+        self.head = self.head.next
+        if self.head:
+            self.head.prev = None
+        else:
+            self.tail = None
+        self.size -= 1
+        return value
 
-    def parse_conj(self):
-        arg1 = self.parse_neg()
-        while self.lexer.get_token().token_type() == TokenType.CONJ:
-            self.lexer.next_token()
-            arg2 = self.parse_neg()
-            et = BinaryExprTree(BinaryOperation.CONJ, arg1, arg2)
-            arg1 = None
-            arg1 = et
-        return arg1
+    def is_empty(self):
+        return self.head is None
 
-    def parse_neg(self):
-         if self.lexer.get_token().token_type() == TokenType.NEG:
-             self.lexer.next_token()
-             arg = self.parse_neg()
-             et = NegExprTree(arg)
-             return et
-         else:
-             return self.parse_primary_expr()
+class LinkedListCalculator:
+    def __init__(self):
+        self.precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '**': 3, '~': 4}
 
-    def parse_primary_expr(self):
-        token_type = self.lexer.get_token().token_type()
-        if token_type == TokenType.OPEN_BR:
-            self.lexer.next_token()
-            et = self.parse_equiv()
-            if self.lexer.get_token().token_type() == TokenType.CLOSE_BR:
-                self.lexer.next_token()
-                return et
+    def _tokenize(self, expr):
+        """Разбивает строку на токены, возвращая нашу Queue."""
+        q = Queue()
+        i = 0
+        while i < len(expr):
+            char = expr[i]
+            if char.isspace():
+                i += 1
+                continue
+            
+            # Унарный минус (по желанию для полноты, как в задании 1)
+            if char == '-':
+                # Если пустая очередь или последний токен - оператор/скобка
+                if q.is_empty() or (self._is_operator(q.tail.value) or q.tail.value == '('):
+                    q.enqueue('~')
+                    i += 1
+                    continue
+
+            if char.isdigit() or char == '.':
+                num = ""
+                while i < len(expr) and (expr[i].isdigit() or expr[i] == '.'):
+                    num += expr[i]
+                    i += 1
+                q.enqueue(num)
+                continue
+            
+            if expr[i:i+2] == '**':
+                q.enqueue('**')
+                i += 2
+                continue
+                
+            q.enqueue(char)
+            i += 1
+        return q
+
+    def _is_operator(self, token):
+        return token in self.precedence
+
+    def to_postfix(self, expression):
+        tokens_q = self._tokenize(expression)
+        output_q = Queue()
+        op_stack = Stack()
+
+        while not tokens_q.is_empty():
+            token = tokens_q.dequeue()
+            
+            if token.replace('.', '', 1).isdigit():
+                output_q.enqueue(token)
+            elif token == '(':
+                op_stack.push(token)
+            elif token == ')':
+                while not op_stack.is_empty() and op_stack.peek() != '(':
+                    output_q.enqueue(op_stack.pop())
+                op_stack.pop() # Удаляем '('
             else:
-                raise EParserError(self.lexer.get_cur(), "Closed bracket expected")
-        elif token_type == TokenType.CONST:
-            const_value = self.lexer.get_token().val
-            expr = ConstExpr(const_value)
-            self.lexer.next_token()
-            return expr
-        elif token_type == TokenType.VAR:
-            var_name = self.lexer.get_token().s
-            expr = VarExpr(var_name)
-            self.lexer.next_token()
-            return expr
-        else:
-            raise EParserError(self.lexer.get_cur(), "Variable or constant expected")
+                while (not op_stack.is_empty() and op_stack.peek() != '(' and
+                       self.precedence.get(op_stack.peek(), 0) >= self.precedence.get(token, 0)):
+                    if token in ('**', '~') and op_stack.peek() == token:
+                        break
+                    output_q.enqueue(op_stack.pop())
+                op_stack.push(token)
+        
+        while not op_stack.is_empty():
+            output_q.enqueue(op_stack.pop())
+
+        # Сборка результата в строку из Queue
+        res_str = ""
+        while not output_q.is_empty():
+            res_str += str(output_q.dequeue()) + " "
+        return res_str.strip()
+
+    def calculate(self, expression):
+        postfix_str = self.to_postfix(expression)
+        tokens = postfix_str.split() # Здесь split допустим для удобства обхода
+        calc_stack = Stack()
+
+        for token in tokens:
+            if token.replace('.', '', 1).isdigit():
+                calc_stack.push(float(token))
+            elif token == '~':
+                calc_stack.push(-calc_stack.pop())
+            else:
+                b = calc_stack.pop()
+                a = calc_stack.pop()
+                if token == '+': calc_stack.push(a + b)
+                elif token == '-': calc_stack.push(a - b)
+                elif token == '*': calc_stack.push(a * b)
+                elif token == '/': calc_stack.push(a / b)
+                elif token == '**': calc_stack.push(a ** b)
+        
+        return float(calc_stack.pop())
+
+# --- ПУНКТ 4: Замеры времени ---
+def benchmark():
+    s = Stack()
+    q = Queue()
+    n = 10000
+    
+    start = time.time()
+    for i in range(n): s.push(i)
+    for i in range(n): s.pop()
+    print(f"Stack 10k push/pop: {time.time() - start:.5f} сек")
+
+    start = time.time()
+    for i in range(n): q.enqueue(i)
+    for i in range(n): q.dequeue()
+    print(f"Queue 10k enq/deq: {time.time() - start:.5f} сек")
+
+benchmark()
+
+# --- ПУНКТ 5: 8 Тестовых выражений ---
+calc = LinkedListCalculator()
+tests = [
+    "2 + 2",
+    "10 - 2 * 3",
+    "(3 + 4) * 2",
+    "2 ** 3 ** 2",
+    "10.5 / (2.1 + 2.9)",
+    "-5 + 10",
+    "((1 + 2) * (3 - 4)) / 2",
+    "2 * 3 + 4 ** 2"
+]
+
+print("\nТестирование выражений:")
+for t in tests:
+    print(f"Инфикс: {t}")
+    print(f"Постфикс: {calc.to_postfix(t)}")
+    print(f"Результат: {calc.calculate(t)}\n")
